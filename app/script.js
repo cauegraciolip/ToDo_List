9ce98a3bc39f5ef3c595f7187f32955b007ed3c1
Caue Gracioli <cauegracioli@outlook.com>
2021-07-27 16:37:53 -0300

    set javascript

---
+++ app/script.js
@@ -0,0 +1,61 @@
+var tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+
+onload = function() {
+
+    let ul = document.getElementById('ul')
+    let content = ""
+
+    function add() {
+
+        tarefas.forEach(element => {
+            content += `<li class="li_tarefas">
+            <p>${element}</p> 
+            <button onclick="deleteItem" class="btn_lista">
+                <i class="fas fa-times empty"></i>
+            </button>
+            </li>`
+        }); 
+        ul.innerHTML = content
+    }
+    add()
+}
+
+
+function insert() {
+
+    let texto = document.getElementById('tasks').value
+
+    if (texto === "") {
+
+        alert("Insira um texto")
+
+    } else {
+
+        tarefas.push(texto)
+        localStorage.setItem('tarefas', JSON.stringify(tarefas))
+        location.reload()
+
+    }
+
+}
+
+function deleteList() {
+
+    localStorage.removeItem('tarefas')
+    location.reload()
+
+}
+
+function contar() {
+
+    let qtd = document.getElementById('footer_p')
+    let qtdItens = tarefas.length
+
+    let content = ""
+
+    content = `${qtdItens} itens left`
+
+    qtd.innerHTML = content
+}
+
+contar()
