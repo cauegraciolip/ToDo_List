f07f14a80c55c1bc4d997733ade1c3871e539df6
Caue Gracioli <cauegracioli@outlook.com>
2021-08-01 19:39:35 -0300

    recolocar tarefa nao concluida

--- app/script.js
+++ app/script.js
@@ -1,6 +1,11 @@
 var tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
 
+var concluidas = JSON.parse(localStorage.getItem('concluidas')) || [];
+
 onload = function() {
+
+    let completed = document.getElementById('conc')
+    let contentDone = ""
 
     let ul = document.getElementById('ul')
     let content = ""
@@ -9,13 +14,149 @@ onload = function() {
 
         tarefas.forEach(element => {
             content += `<li class="li_tarefas">
-            <p>${element}</p><i class="fas fa-times empty"></i></li>`
+            <div class="lista">
+            <button type="submit" class="done" onclick="completed(${element.id})"><i class="fas fa-check"></i></button>
+            <p class="texto">${element.tarefa}</p>
+            </div>
+            <i class="fas fa-times empty" onclick="deleteItem(${element.id})"></i>
+            </li>`
         }); 
         ul.innerHTML = content
     }
     add()
+
+    function completar() {
+
+        concluidas.forEach(element => {
+            contentDone += `<li class="li_tarefas_concluidas">
+            <div class="lista">
+            <button type="submit" class="done" onclick="noncompleted(${element.id})"><i class="fas fa-check"></i></button>
+            <p class="texto">${element.tarefa}</p>
+            </div>
+            <i class="fas fa-times empty" onclick="deleteItemCompleted(${element.id})"></i>
+            </li>`
+        }); 
+        completed.innerHTML = contentDone
+    }
+    completar()
 }
 
+function completed(id) {
+
+    let nova_lista_sem_click = []
+
+    for ( i = 0; i < tarefas.length; i++ ) {
+
+        if ( tarefas[i].id == id ) {
+
+            task = tarefas[i].tarefa
+
+            let tarefa_concluida = {
+
+                id: Math.random() * 10,
+
+                tarefa: task,
+
+                concluida: true
+
+            }
+
+            concluidas.push(tarefa_concluida)
+            localStorage.setItem('concluidas', JSON.stringify(concluidas))
+            
+        } else {
+
+            nova_lista_sem_click.push(tarefas[i])
+
+        }
+
+        
+        localStorage.setItem('tarefas', JSON.stringify(nova_lista_sem_click))
+        
+
+    }
+
+    
+    location.reload()
+
+}
+
+function deleteItem(id) {
+
+    let nova_lista_sem_click = []
+
+    for ( let i = 0; i < tarefas.length; i++ ) {
+
+        if ( tarefas[i].id != id ) {
+
+            nova_lista_sem_click.push(tarefas[i])
+
+        }
+
+    }
+
+    localStorage.setItem('tarefas', JSON.stringify(nova_lista_sem_click))
+    location.reload()
+}
+
+function deleteItemComplete(id) {
+
+    let nova_lista_sem_click = []
+
+    for ( let i = 0; i < concluidas.length; i++ ) {
+
+        if ( concluidas[i].id != id ) {
+
+            nova_lista_sem_click.push(concluidas[i])
+
+        }
+
+    }
+
+    localStorage.setItem('concluidas', JSON.stringify(nova_lista_sem_click))
+    location.reload()
+
+}
+
+function noncompleted(id) {
+
+    let nova_lista_sem_click = []
+
+    for ( i = 0; i < concluidas.length; i++ ) {
+
+        if ( concluidas[i].id == id ) {
+
+            task = concluidas[i].tarefa
+
+            let tarefa_naoConcluida = {
+
+                id: Math.random() * 10,
+
+                tarefa: task,
+
+                concluida: false
+
+            }
+
+            tarefas.push(tarefa_naoConcluida)
+            localStorage.setItem('tarefas', JSON.stringify(tarefas))
+            
+        } else {
+
+            nova_lista_sem_click.push(concluidas[i])
+
+        }
+
+        
+        localStorage.setItem('concluidas', JSON.stringify(nova_lista_sem_click))
+        
+
+    }
+
+    
+    location.reload()
+
+}
 
 function insert() {
 
@@ -27,9 +168,19 @@ function insert() {
 
     } else {
 
-        tarefas.push(texto)
-        localStorage.setItem('tarefas', JSON.stringify(tarefas))
-        location.reload()
+        let nova_tarefa = {
+
+            id: Math.random() * 10,
+
+            tarefa: texto,
+
+            concluida: false
+
+        }
+
+    tarefas.push(nova_tarefa)
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+    location.reload()
 
     }
 
@@ -38,20 +189,25 @@ function insert() {
 function deleteList() {
 
     localStorage.removeItem('tarefas')
+    localStorage.removeItem('concluidas')
     location.reload()
 
 }
 
-function contar() {
+function deleteCompleted() {
 
-    let qtd = document.getElementById('footer_p')
-    let qtdItens = tarefas.length
+    localStorage.removeItem('concluidas')
+    location.reload()
 
-    let content = ""
-
-    content = `${qtdItens} itens left`
-
-    qtd.innerHTML = content
 }
 
-contar()
+let qtd = document.getElementById('footer_p')
+let qtdItens = tarefas.length
+
+let content = ""
+
+content = `${qtdItens} itens left`
+
+qtd.innerHTML = content
+
+
